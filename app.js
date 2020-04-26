3305ef9fc7bd6ddce45879eab78e69e352bc3e25
jwang1 <sigkdd@yahoo.com>
2020-04-26 17:13:21 -0400

    Understand TypeScript's enum Transpiled to function and those values :  ENV['DEV'] is a number 0;  ENV[ENV['DEV']] is the string-value 'DEV'

--- app.js
+++ app.js
@@ -11,3 +11,22 @@ var aTestEnv = 'qa';
 var aProdEnv = "prod";
 console.log("type of aTestEnv: " + typeof (aTestEnv));
 console.log("type of aProdEnv: " + typeof (aProdEnv));
+var ENVS;
+(function (ENVS) {
+    ENVS[ENVS["DEV"] = 0] = "DEV";
+    ENVS[ENVS["QA"] = 1] = "QA";
+    ENVS[ENVS["PC"] = 2] = "PC";
+    ENVS[ENVS["PROD"] = 3] = "PROD";
+
+    // understand the TypeScript enum to JavaScript's function
+    console.log("________ inside Transpiled JavaScript ______");
+    console.log("ENVS['DEV']] = " + ENVS['DEV']);
+    console.log("ENVS[ENVS['DEV']]] = " + ENVS[ENVS['DEV']]);
+
+})(ENVS || (ENVS = {}));
+;
+console.log("type of TypeScript's enum: " + typeof (ENVS.PROD));
+console.log("value of TypeScript enum: " + ENVS.PROD);
+console.log("value of TypeScript enum: " + ENVS.NONE);
+
+console.log("what's TypeScript enum - ENVS : " + ENVS);
--- app.ts
+++ app.ts
@@ -12,7 +12,7 @@ const s = add(a, b);
 console.log("sum of " + a + " and " + b + " is: " + s);
 
 
-// check the custom type
+// check the custom type (following type, is just string type with limited allowed values)
 type nonProdEnv = 
     'dev'
     | 'qa'
@@ -30,4 +30,15 @@ const aProdEnv: prodEnv = "prod";
 console.log("type of aTestEnv: " + typeof(aTestEnv));
 console.log("type of aProdEnv: " + typeof(aProdEnv));
 
+enum ENVS {
+  DEV,
+  QA,
+  PC,
+  PROD,
+};
+
+console.log("type of TypeScript's enum: " + typeof(ENVS.PROD));
+console.log("value of TypeScript enum: " + ENVS.PROD);
+
+console.log("what's TypeScript enum - ENVS : " + ENVS)
 
