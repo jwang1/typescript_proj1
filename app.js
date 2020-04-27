cd4bee2708e53f5a629a559f7c81d92c969f2c45
jwang1 <sigkdd@yahoo.com>
2020-04-27 10:50:58 -0400

    add the source map files -- TS creates for the debug

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
+"use strict";
 console.log("Understanding TypeScript");
 console.log("testing the liver deployment.");
 function add(a, b) {
     return a + b;
 }
-var a = 3.5;
-var b = 5;
-var s = add(a, b);
+const a = 3.5;
+const b = 5;
+const s = add(a, b);
 console.log("sum of " + a + " and " + b + " is: " + s);
-var aTestEnv = 'qa';
-var aProdEnv = "prod";
+const aTestEnv = 'qa';
+const aProdEnv = "prod";
 console.log("type of aTestEnv: " + typeof (aTestEnv));
 console.log("type of aProdEnv: " + typeof (aProdEnv));
 var ENVS;
@@ -17,17 +18,9 @@ var ENVS;
     ENVS[ENVS["QA"] = 1] = "QA";
     ENVS[ENVS["PC"] = 2] = "PC";
     ENVS[ENVS["PROD"] = 3] = "PROD";
-
-    // understand the TypeScript enum to JavaScript's function
-    console.log("________ inside Transpiled JavaScript ______");
-    console.log("ENVS['DEV']] = " + ENVS['DEV']);
-    console.log("ENVS[ENVS['DEV']]] = " + ENVS[ENVS['DEV']]);
-    console.log("what's inside this object ENVS? : " + ENVS);
-
 })(ENVS || (ENVS = {}));
 ;
 console.log("type of TypeScript's enum: " + typeof (ENVS.PROD));
 console.log("value of TypeScript enum: " + ENVS.PROD);
-console.log("value of TypeScript enum: " + ENVS.NONE);
-
 console.log("what's TypeScript enum - ENVS : " + ENVS);
+//# sourceMappingURL=app.js.map
--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-var john = {
+"use strict";
+let john = {
     age: 23,
     name: 'John',
     talk: function () {
         return "my name is: " + this.name + ", " + this.age + " old, ";
-    }
+    },
 };
 function talkOut(person) {
     return person.talk();
 }
 console.log(talkOut(john));
+//# sourceMappingURL=main.js.map
